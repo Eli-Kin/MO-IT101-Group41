01efeacc725f159d6ec07b1f89e18ae23bee83ff
azloserbits <ribbun.draws@gmail.com>
2026-03-13 13:01:40 +0800

    re-org variables + split methods + added termination

--- src/main/java/com/motorph/Main.java
+++ src/main/java/com/motorph/Main.java
@@ -17,7 +17,8 @@ public class Main {
     public static final String RED = "\u001B[31m";
     public static final String BLUE = "\u001B[34m";
 
-    //template
+
+    //template for more colours
     //\u001B + [xx, where xx is code.
     public static final String TEMPLATE = "\u001B[34m";
 
